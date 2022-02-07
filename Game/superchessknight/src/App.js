904b501704729ce0ac62a8f78c71001ba5657888
d1joseph <dhiv.joseph@gmail.com>
2022-02-07 19:17:47 +1100

    project init, React and Rails setup.

---
+++ Game/superchessknight/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {} from './containers';
+// import {} from './components';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default App
